ece18df822030fe617997ba89a2cfbe036cc5c2d
Sally Diament <sally.barzilai@gmail.com>
2023-01-13 14:57:11 -0300

    configurando server, app e criando ICar

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,10 @@
+interface ICar {
+    id?: string;
+    model: string;
+    year: number,
+    color: string;
+    status: boolean;
+    buyValue: number;
+    doorsQty: number;
+    seatsQty: number;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+// app.use(routes);
+// app.use(ErrorHandler.handle);
 
 export default app;
