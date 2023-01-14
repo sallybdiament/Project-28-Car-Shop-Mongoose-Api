19a01f58762ea4416efc736f11a9a08be1fc08a0
Sally Diament <sally.barzilai@gmail.com>
2023-01-14 01:16:53 -0300

    atualizando

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -9,8 +9,7 @@ import {
 import ErrorResponse from '../Middleware/ErrorResponse';
     
 const messageInvalidMongoId = 'Invalid mongo id';
-// const carNotFound = 'Car not found';
-const motoNotFound = 'Motorcycle not found';
+
 abstract class AbstractODM<T> {
   protected model: Model<T>;
   protected schema: Schema;
@@ -45,8 +44,8 @@ abstract class AbstractODM<T> {
     const check = regex.test(_id);
     if (!check) throw new ErrorResponse(422, messageInvalidMongoId);
     // if (!isValidObjectId(_id)) throw Error('Car not found');
-    const find = await this.model.find({ _id });
-    if (find.length === 0) throw new ErrorResponse(404, motoNotFound);
+    // const find = await this.model.find({ _id });
+    // if (find.length === 0) throw new ErrorResponse(404, motoNotFound);
     const updated = await this.model.findByIdAndUpdate(
       { _id },
       { ...obj } as UpdateQuery<T>,
@@ -57,8 +56,8 @@ abstract class AbstractODM<T> {
 
   public async delete(_id: string) {
     if (!isValidObjectId(_id)) throw new ErrorResponse(422, 'Invalid Mongo id');
-    const find = await this.model.find({ _id });
-    if (find.length === 0) throw new ErrorResponse(404, motoNotFound);
+    // const find = await this.model.find({ _id });
+    // if (find.length === 0) throw new ErrorResponse(404, motoNotFound);
     return this.model.deleteOne({ _id });
   }
 }
